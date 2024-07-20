0046d1ddfbbd8741dd51f27c69485ddd52274d57
Diego Melnek <diego.melnek@gmail.com>
2024-07-20 19:28:15 -0300

    Add README.txt

---
+++ src/set/Pesquisa/ListaTarefas.java
@@ -0,0 +1,90 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaTarefas {
+    private Set<Tarefa> tarefaSet;
+
+    public ListaTarefas() {
+        this.tarefaSet = new HashSet<>();
+
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaSet.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao) {
+        if (!tarefaSet.isEmpty()) {
+            if (tarefaSet.contains(descricao)) {
+                tarefaSet.remove(descricao);
+            }else {
+                System.out.println("Tarefa não localizada");
+        }
+        }else {
+            System.out.println("Tarefa não listada");
+        }
+    }
+
+    public void exibirTarefas() {
+        System.out.println(tarefaSet);
+    }
+
+    public int contarTarefas() {
+        return tarefaSet.size();
+    }
+
+    public Set<Tarefa> obterTarefasConcluidas() {
+        Set<Tarefa> tarefasConcluidas = new HashSet<>();
+        for (Tarefa t : tarefaSet) {
+            if (t.isConcluida()) {
+                tarefasConcluidas.add(t);
+            }
+        }
+        return tarefasConcluidas;
+    }
+
+    public Set<Tarefa> obterTarefasPendentes() {
+        Set<Tarefa> tarefasPendentes = new HashSet<>();
+        for (Tarefa t : tarefaSet) {
+            if (!t.isConcluida()) {
+                tarefasPendentes.add(t);
+            }
+        }
+        return tarefasPendentes;
+    }
+
+    public Set<Tarefa> marcarTarefaConcluida(String descricao) {
+        for (Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                t.setConcluida(true);
+            }
+        }
+    }
+    public Set<Tarefa> marcarTarefaPendente(String descricao) {
+        Tarefa tarefaParaMarcarComoPendente = null;
+        for (Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaMarcarComoPendente = t;
+                break;
+            }
+        }
+
+        if (tarefaParaMarcarComoPendente != null()) {
+            if (tarefaParaMarcarComoPendente.isConcluida()) {
+                tarefaParaMarcarComoPendente.setConcluida(false);
+            }
+        }else {
+            System.out.println("Tarefa não localizada");
+        }
+
+    }
+
+    public void limparListaTarefas() {
+        if (tarefaSet.isEmpty()) {
+            System.out.println("A lista está vazia");
+        } else {
+            tarefaSet.clear();
+        }
+    }
+}
